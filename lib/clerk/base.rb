7d570ce771c0cc0a0222f05658c5ee280bbe7847
doomspork <iamdoomspork@gmail.com>
2013-05-20 14:45:43 -0700

    Removed parser functionality from Clerk

--- lib/clerk/base.rb
+++ lib/clerk/base.rb
@@ -6,25 +6,28 @@ module Clerk
       @transformed_values = Array.new
     end
 
-    def load(path)
-      @raw_values = self.class.parser.parse(path).to_enum
+    def load(data)
+      @raw_values = data.freeze
     end
 
     def results
       Enumerator.new do |yielder|
-        @raw_values.each_with_index do |value, index|
-          yielder << @transformed_values[index] ||= self.class.apply_template(value)
+        @transformed_values.each do |value|
+          yielder << value
         end
       end
     end
 
-    def self.apply_template(data)
-      # Apply template through the transformer
+    def errors
+      @template.errors || []
     end
 
-    ##
-    # Configuration helpers
-    ##
+    def self.apply_template(data)
+      @raw_values.each_with_index do |value, index|
+        @transformed_values[index] ||= @template.apply(value)
+      end
+    end
+
     def self.template
       @template ||= Clerk::Template.new 
       #We could do `block_given?` here but it might be better to let it error if nothing is provided
@@ -32,14 +35,5 @@ module Clerk
       @template
     end
 
-    def self.parser(parser, *options)
-      parser = Clerk::ParserRegistry.lookup(parser.to_sym) if [Symbol, String].any? { |t| parser.kind_of? t }
-      #TODO Should we enforce Clerk::Parser subclassing here?
-      @parser = parser.new(options)
-    end
-
-    def self.parser
-      @parser
-    end
   end
 end
