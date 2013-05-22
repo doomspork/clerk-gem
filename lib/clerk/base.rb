ba136e53bc53e711c613a01f896d9df9af6ebce4
doomspork <iamdoomspork@gmail.com>
2013-05-21 18:53:08 -0700

    ActiveModel validation support, hooray
    - Added ResultSet to hold a single record of data, necessary for
      validators
    - Flatten objects with groups
    - Handle .valid? in Base by passing on to ResultSets
    - Aggregate error messages from ResultSets

--- lib/clerk/base.rb
+++ lib/clerk/base.rb
@@ -1,38 +1,101 @@
 module Clerk
+  class ResultSet
+    include ActiveModel::Validations
+
+    def initialize(data)
+      @data = data.freeze
+    end
+
+    def read_attribute_for_validation(attribute)
+      @data[attribute]
+    end
+
+    def self.name
+      self.class.to_s
+    end
+
+    def self.copy_validations_from(klass)
+      dup = klass.validators.dup
+      dup.each do |validator|
+        validates_with validator.class, validator.options.dup.merge({:attributes => validator.attributes.dup})
+      end
+    end
+  end
+
   class Base
     include ActiveModel::Validations
 
+    class << self
+      attr_accessor :_result_set_klass
+    end
+
     def initialize
-      @transformed_values = Array.new
+      clear_transformed_data!
+      result_klass = Class.new(Clerk::ResultSet)
+      result_klass.copy_validations_from(self.class)
+      self.class._result_set_klass = result_klass
     end
 
     def load(data)
-      @raw_values = data.freeze
+      clear_transformed_data!
+      raw_values = data.freeze
+      @transformed_values = raw_values.map do |record|
+        templated_data = self.class.apply_template(record) 
+        transformed_data = self.class.apply_transformations(templated_data)
+        self.class.result_sets(transformed_data)
+      end
+      self
     end
 
-    def results
-      Enumerator.new do |yielder|
-        @transformed_values.each do |value|
-          yielder << value
-        end
+    def valid?
+      @transformed_values.all? do |sets| 
+        sets.all? { |set| set.valid? } 
       end
     end
 
     def errors
-      @template.errors || []
+      error_messages = Hash.new
+      @transformed_values.each_with_index do |sets, index|
+        messages = sets.map { |set| set.errors.full_messages }.flatten.uniq
+        error_messages[index + 1] = messages unless messages.empty?
+      end
+      error_messages
     end
 
     def self.apply_template(data)
-      @raw_values.each_with_index do |value, index|
-        @transformed_values[index] ||= @template.apply(value)
-      end
+      data
+    end
+
+    def self.apply_transformations(data)
+      data
     end
 
     def self.template
       @template ||= Clerk::Template.new 
-      #We could do `block_given?` here but it might be better to let it error if nothing is provided
-      yield @template 
+      yield @template if block_given?
       @template
+    end
+
+    private
+
+    def self.result_sets(record)
+      data = record.freeze
+      sets = Array.new
+      if template.has_grouped_element?
+        grouped_data = data.reject! { |key, value| value.kind_of? Clerk::TemplateGroup }
+        grouped_data.each do |key, values|
+          values.each do |value|
+            sets.push _result_set_klass.new(data.merge(value))
+          end
+        end
+      else
+        sets.push _result_set_klass.new(data)
+      end
+      sets
+    end
+
+    def clear_transformed_data!
+      @transformed_values = Array.new
     end
 
   end
